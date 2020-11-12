d85b7fa12db8d13f2f23fa4b471af7fa85b2e855
Jaroslav Beredi <xarocx@gmail.com>
2020-11-12 20:30:54 +0100

    updated footer a o-nas

--- o-nas.php
+++ o-nas.php
@@ -10,7 +10,7 @@ include 'includes/header.php';
         <div class="row">
             <div class="col-lg-12">
                 <h1>O nás<br>
-                    <small>O Kutúrno umeleckom spolku Jána Kollára.</small></h1>
+                    <small>Niekoľko slov o našom spolku.</small></h1>
             </div>
         </div>
     <img src="images/kus2020.jpg" title="Členovia 2020" alt="clenovia spolku 2020" style="width: 100%;">
@@ -25,7 +25,7 @@ include 'includes/header.php';
             <p class="text-justify">V rámci činnosti KUS pracovali: divadelná, tanečná, hudobno-spevácka, výtvarná sekcia a knižnica. V spomenutých oblastiach sa striedali početní selenčskí ochotníci, ktorí prispeli do kultúrneho bohatstva Selenče. Cieľom vždy bolo zachovať zvyky, obyčaje a tradície, ktoré boli nacvičované v podobe tanečných choreografií a následne vhodne spracované pre javiskové predvedenie.</p>
             <p class="text-justify">Katarína Berédiová venovala svoj voľný čas spolku od roku 1956. Dlhé roky bola vedúcou tanečných skupín a aktívnou členkou divadelnej sekcie.</p>
             <p class="text-justify">KUS bol nositeľom <span class="italic">Októbrovej ceny</span> obce Báč v roku 1975 a ceny <span class="italic">Iskra kultúry</span> Kultúrno osvetového spoločenstva SAPV v roku 1978, pod vedením Kataríny Berédiovej.</p>
-            <p class="text-justify">Dnes spolok počíta okolo 40 členov rôznych vekových kategórií. Najpočetnejšia je tanečná skupina, kde je umeleckým vedúcim Marijan Trusina. Divadelná sekcia pracuje príležitostne, kedy prispieva do kultúrno umeleckých programov rôznymi scénkami. Pod vedením Anny Berédiovej pracuje dievčenská spevácka skupina.</p>
+            <p class="text-justify">Dnes spolok počíta okolo 40 členov rôznych vekových kategórií. Najpočetnejšia je tanečná skupina, kde je umeleckým vedúcim Jaroslav Berédi. Divadelná sekcia pracuje príležitostne, keď prispieva do kultúrno-umeleckých programov rôznymi scénkami. Pod vedením Jaroslava Berédiho a Juraja Súdiho ml. pracuje dievčenská spevácka skupina. Juraj Súdi ml. má na starosti aj orchester.</p>
             <p class="text-justify">
                 Tanečná a spevácka skupina sa pravidelne zúčastňujú folklórneho festivalu <span class="italic">Tancuj, tancuj...</span> v Hložanoch. Taktiež vystupujú na rôznych festivaloch a programoch, ako doma, tak aj v zahraničí.
             </p>
