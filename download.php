cf542dc6db03df619a96b1eb09e1857be7945261
Jaroslav Beredi <xarocx@gmail.com>
2021-01-14 18:18:24 +0100

    download

--- download.php
+++ download.php
@@ -8,14 +8,19 @@ include 'includes/header.php';
     include 'includes/navbar.php';
     ?>
 </div>
+<!--TODO: Dynamic files upload/read-->
 <div class="container">
     <h1>Na stiahnutie<br>
     <small>Pozrite si naše publikácie a promo materiál.</small></h1>
     <div class="row photoGallery">
         <div class="col-lg-3">
             <div class="thumbnail text-center">
-                <a href="#"><img src="images/malatina-2017/1.jpg" title="Stiahnuť" alt="Cinnost sprevadzana hudbou" class="galleryItem"> </a>
-                <a href="malatina-2017.php" title="Stiahnuť"><p>Činnosť sprevádzaná hudbou (.PDF)</p></a>
+                <a href="images/files/cinnost-sprevadzana-hudbou.pdf">
+                    <img src="images/pdf-icon.jpg" title="Stiahnuť" alt="Cinnost sprevadzana hudbou" style="width: 100px;">
+                </a>
+                <a href="images/files/cinnost-sprevadzana-hudbou.pdf" title="Stiahnuť">
+                    <p>Činnosť sprevádzaná hudbou (.PDF)</p>
+                </a>
             </div>
         </div>
     </div>
@@ -34,7 +39,6 @@ footer("other");
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="js/bootstrap.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="lightbox/js/lightbox.js"></script>
 
 <script src="links/link.js"></script>
 </body>
