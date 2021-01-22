368be3174ae3468777659d5af8e79d1bc9b60453
Jaroslav Beredi <xarocx@gmail.com>
2021-01-22 14:58:54 +0100

    Navbar->Sekcie dynamicky

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,38 +1,51 @@
-<div class="row top navbar" >
-    <div class="col-lg-1 col-lg-offset-0 col-md-1 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3" >
+<div class="row top navbar">
+    <div class="col-lg-1 col-lg-offset-0 col-md-1 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
 
-        <img class="img-responsive" src = "images/znak.jpg"  title = "Znak" alt = "Znak" >
-    </div >
+        <img class="img-responsive" src = "images/znak.jpg"  title = "Znak" alt = "Znak">
+    </div>
     <div class="col-lg-4 hidden-md hidden-sm hidden-xs headerText top">
         <p> KULTÚRNO UMELECKÝ SPOLOK <br>
-            <span class="bold" > JÁNA KOLLÁRA </span > SELENČA</p>
-    </div >
+            <span class="bold"> JÁNA KOLLÁRA </span> SELENČA</p>
+    </div>
     <div class="hidden-lg col-md-3 hidden-sm hidden-xs headerTextMd topMd">
         <p> KULTÚRNO UMELECKÝ SPOLOK <br>
-            <span class="bold" > JÁNA KOLLÁRA </span > SELENČA</p>
-    </div >
+            <span class="bold"> JÁNA KOLLÁRA </span> SELENČA</p>
+    </div>
     <div class="hidden-lg hidden-md col-sm-12 col-xs-12 headerTextSm text-center top">
         <p> KULTÚRNO UMELECKÝ SPOLOK <br>
-            <span class="bold" > JÁNA KOLLÁRA </span > SELENČA</p>
-    </div >
+            <span class="bold"> JÁNA KOLLÁRA </span> SELENČA</p>
+    </div>
     <div class="col-lg-7 col-md-8 col-sm-12 col-xs-12 top">
         <ul class="nav navbar-nav" style="widows: 100%;">
-            <li ><a href = "index" ><span class="glyphicon glyphicon glyphicon-home" ></span> ÚVOD</a></li >
+            <li><a href = "index"><span class="glyphicon glyphicon glyphicon-home"></span> ÚVOD</a></li>
             <li><a href="about?pagepseu=about"> O NÁS </a></li>
             <li class="dropdown">
-                <a href = "#" class="dropdown" data-toggle = "dropdown"> SEKCIE <span class="caret" ></span ></a >
-                <ul class="dropdown-menu" >
-                    <li><a href = "about?pagepseu=folklor" > Folklórna sekcia </a ></li >
-                    <li><a href = "about?pagepseu=divadlo" > Divadelná sekcia </a > </li >
-                </ul >
-            </li >
-            <li class="dropdown" ><a href = "#" class="dropdown-toggle" data-toggle = "dropdown" > PROMO <span class="caret" ></span ></a >
-                <ul class="dropdown-menu" >
-                    <li><a href = "videogallery" > Videogaléria</a > </li >
-                    <li><a href = "download" > Na stiahnutie</a > </li >
+                <a href = "#" class="dropdown" data-toggle = "dropdown"> SEKCIE <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+	                <?php
+	                include 'dashboard/includes/db.php';
+
+	                $query = "SELECT * from pages";
+
+	                $send_info = $connection->prepare($query);
+
+	                $send_info->execute();
+	                while ($row = $send_info->fetch(PDO::FETCH_ASSOC)) {
+	                    if($row['page_pseu'] == 'about'){continue;}
+		                ?>
+                        <li><a href = "about?pagepseu=<?=$row['page_pseu']?>"> <?=$row['page_title']?></a></li>
+		                <?php
+	                }
+	                ?>
                 </ul>
             </li>
-            <li class="dropdown" ><a href = "#" class="dropdown-toggle" data-toggle = "dropdown" > PODUJATIA <span class="caret" ></span ></a >
+            <li class="dropdown"><a href = "#" class="dropdown-toggle" data-toggle = "dropdown"> PROMO <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href = "videogallery"> Videogaléria</a> </li>
+                    <li><a href = "download"> Na stiahnutie</a> </li>
+                </ul>
+            </li>
+            <li class="dropdown"><a href = "#" class="dropdown-toggle" data-toggle = "dropdown"> PODUJATIA <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="nadchadzajuce-podujatia"> Nadchádzajúce podujatia </a></li>
                     <li><a href="predchadzajuce-podujatia"> Predchádzajúce podujatia </a></li>
