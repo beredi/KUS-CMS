f13c2950ec55366576ef3e6b5e0a7d74cfe27b12
Jaroslav Beredi <xarocx@gmail.com>
2021-02-08 21:29:10 +0100

    videogalerry

--- videogallery.php
+++ videogallery.php
@@ -11,48 +11,36 @@ include 'includes/header.php';
         <h1>Videogaléria<br>
             <small>Videá z našich vystúpení.</small></h1>
         <div class="row photoGallery">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/suxytogt2i4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Choreografia: <span class="italic">Z teho konca na tem koniec</span> | Choreografi: Koletív</p>
+
+	        <?php
+
+	        try{
+
+		        include 'dashboard/includes/db.php';
+		        $query = "SELECT * FROM videogallery ORDER BY id DESC";
+
+		        $send_info = $connection->prepare($query);
+
+		        $send_info->execute();
+
+		        while ($row = $send_info->fetch(PDO::FETCH_ASSOC)){
+		            ?>
+
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="thumbnail text-center" style="height: 450px;">
+                        <iframe width="100%" height="400px" <?=$row['code']?>></iframe>
+                        <p><?=$row['title']?></p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/lt9Zz0Nmf1g" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Choreografia: <span class="italic">Kyjáčkový tanec</span> | Choreografka: Katarína Berédiová, úprava: Jaroslav Beredi</p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/OLBqJ9tsRMo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <p>Choreografia: <span class="italic">Kyjáčkový tanec</span> | Choreografka: Katarína Berédiová</p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/kON18J1pYyw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Choreografia: <span class="italic">Žeň sa, pajtáš</span> | Choreografka: Malvína Zolňanová</p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/ls1QbdNEV7Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Dievčenská spevácka skupina - Zmes - <span class="italic">Veje vetor hore dolinami</span> </p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/VDGvhhC0ON4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Dievčenská spevácka skupina - Zmes - <span class="italic">Pod oblôčkom záhradôčka</span> </p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="thumbnail text-center">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/CKL4ZkFoH1w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p>Choreografia: <span class="italic">V Selenči na tanci</span> | Choreografi: Tanečníci</p>
-                </div>
-            </div>
+            <?php
+		        }
+
+	        }
+	        catch (Exception $e){
+		        echo $e;
+	        }
+
+	        ?>
         </div>
 
     </div>
