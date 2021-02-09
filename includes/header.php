afb87b5e08f8fb38460d86b7a26408905532af63
Jaroslav Beredi <xarocx@gmail.com>
2021-02-09 10:43:25 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -58,4 +58,6 @@ session_start();
     <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
+    <!--  TODO Lazy loading content  --->
+
 </head>
