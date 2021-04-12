39a7bffa636b020a7a3f0d60c9263d010bf5de47
Jaroslav Beredi <jaroslav.beredi@m2ms.sk>
2021-04-12 10:01:34 +0200

    SQL Insert into videogallery for localhost

---
+++ databasetools/scripts/2021-04-12_Insert_Videogallery.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `videogallery` (`id`, `title`, `code`) VALUES
+(1, 'Choreografia: V Selenči na tanci | Choreografi: Kolektív', 'src=\"https://www.youtube.com/embed/CKL4ZkFoH1w\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(2, 'Dievčenská spevácka skupina - Zmes - Pod oblôčkom záhradôčka', 'src=\"https://www.youtube.com/embed/VDGvhhC0ON4\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(3, 'Dievčenská spevácka skupina - Zmes - Veje vetor hore dolinami', 'src=\"https://www.youtube.com/embed/ls1QbdNEV7Q\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(4, 'Choreografia: Žeň sa, pajtáš | Choreografka: Malvína Zolňanová', 'src=\"https://www.youtube.com/embed/kON18J1pYyw\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(5, 'Choreografia: Kyjáčkový tanec | Choreografka: Katarína Berédiová', 'src=\"https://www.youtube.com/embed/OLBqJ9tsRMo\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen'),
+(6, 'Choreografia: Kyjáčkový tanec | Choreografka: Katarína Berédiová, úprava: Jaroslav Beredi', 'src=\"https://www.youtube.com/embed/lt9Zz0Nmf1g\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(7, 'Choreografia: Z teho konca na tem koniec | Choreografi: Koletív', 'src=\"https://www.youtube.com/embed/suxytogt2i4\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen'),
+(8, 'Dievčenská spevácka skupina a ĽH Selenča - Mám pred domom', 'src=\"https://www.youtube.com/embed/56aXn-6nBRg\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen');
