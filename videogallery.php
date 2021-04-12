b74104167c063da7b7b83d874b1380d24b1ae24b
Jaroslav Beredi <jaroslav.beredi@m2ms.sk>
2021-04-12 10:10:30 +0200

    Pagination videogallery

--- videogallery.php
+++ videogallery.php
@@ -8,21 +8,85 @@ include 'includes/header.php';
     ?>
     <!--CONTENT-->
     <div class="container">
-        <h1>Videogaléria<br>
-            <small>Videá z našich vystúpení.</small></h1>
-        <div class="row photoGallery">
 
 	        <?php
 
-	        try{
-
+	        try {
 		        include 'dashboard/includes/db.php';
-		        $query = "SELECT * FROM videogallery ORDER BY id DESC";
+		        $posts_by_page = 6;
+		        // POSTS COUNTER
+		        $queryCounter = "SELECT * FROM videogallery";
+		        $send_info = $connection->prepare($queryCounter);
+		        $send_info->execute();
+		        $countPosts = $send_info->rowCount();
+		        $countPages = ceil($countPosts / $posts_by_page);
+
+
+		        if (isset($_GET['page'])) {
+			        $page = $_GET['page'];
+		        } else {
+			        $page = '';
+		        }
+
+		        if ($page == '' || $page == 1) {
+			        $page_1 = 0;
+			        $currentPage = 1;
+		        } else {
+			        $page_1 = ($page * $posts_by_page) - $posts_by_page;
+			        $currentPage = $page;
+		        }
+	        }
+	        catch (Exception $e) {
+	            echo  $e;
+	        }
+		        ?>
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="float-left">Videogaléria<br>
+                    <small class="float-left">Videá z našich vystúpení.</small></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-offset-10 col-lg-2">
+                <p class="text-right text-muted">Strana <?=$currentPage?> z <?=$countPages?></p>
+            </div>
+        </div>
+        <div class="row photoGallery">
+            <?php
+            try {
+	            include 'dashboard/includes/db.php';
+	            $posts_by_page = 6;
+	            // POSTS COUNTER
+	            $queryCounter = "SELECT * FROM videogallery";
+	            $send_info = $connection->prepare($queryCounter);
+	            $send_info->execute();
+	            $countPosts = $send_info->rowCount();
+	            $countPosts = ceil($countPosts / $posts_by_page);
+
+	            // GET POSTS
+
+	            if(isset($_GET['page'])){
+		            $page = $_GET['page'];
+	            }
+	            else{
+		            $page = '';
+	            }
+
+	            if($page == '' || $page == 1){
+		            $page_1 = 0;
+	            }
+	            else{
+		            $page_1 = ($page * $posts_by_page) - $posts_by_page;
+	            }
+		        $query = "SELECT * FROM videogallery ORDER BY id DESC LIMIT ".$page_1.", ".$posts_by_page;
 
 		        $send_info = $connection->prepare($query);
+	            $count = $send_info->rowCount();
 
 		        $send_info->execute();
-
+	            if($count==0){
+		            echo "<h3>Žiadne videá na zobrazenie.</h3>";
+	            }
 		        while ($row = $send_info->fetch(PDO::FETCH_ASSOC)){
 		            ?>
 
@@ -42,6 +106,30 @@ include 'includes/header.php';
 
 	        ?>
         </div>
+        <div class="row" style="margin-top: 20px">
+            <ul class="pager">
+			    <?php
+			    $page = 1;
+			    if(isset($_GET['page'])){
+				    $page = $_GET['page'];
+			    }
+			    for ($i = 1; $i <= $countPages; $i++){
+				    if ($i == $page){
+					    ?>
+                        <li><a href="videogallery?page=<?=$i?>" class="active-page"><?=$i?></a></li>
+					    <?php
+				    }
+				    else{
+					    ?>
+                        <li><a href="videogallery?page=<?=$i?>"><?=$i?></a></li>
+					    <?php
+				    }
+			    }
+			    ?>
+            </ul>
+
+        </div>
+
 
     </div>
 </div>
