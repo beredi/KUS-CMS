9f823380d5084e321de904a97c0d926aee776edb
beredi <43635436+beredi@users.noreply.github.com>
2022-06-07 16:03:23 +0200

    nadchadzajuce podujatia - do not show passed events #2

--- nadchadzajuce-podujatia.php
+++ nadchadzajuce-podujatia.php
@@ -17,7 +17,7 @@ include 'includes/header.php';
     try{
         include 'dashboard/includes/db.php';
 
-        $query = "SELECT * FROM events ORDER BY event_date ASC";
+        $query = "SELECT * FROM events WHERE DATE(event_date) >= curdate() ORDER BY event_date ASC";
 
         $send_info = $connection->prepare($query);
         $send_info->execute();
